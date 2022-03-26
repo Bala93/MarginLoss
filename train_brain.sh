1d38bc24a44c1cedfd3a5796b7434b765eaf0cc6
bala93 <balamuralim.1993@gmail.com>
2022-03-26 13:34:44 -0400

    Add brain-brats setup

---
+++ train_brain.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="brain" data.batch_size="16" model="unet" model.num_classes="4" model.num_inp_channels="4" optim="adam" scheduler="step" wandb.project="unet-brain" log_period="100"
+#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="cardiac" model="unet" model.num_classes="4" loss="focal" optim="adam" scheduler="step" wandb.project="unet-cardiac"
+#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="cardiac" model="unet" model.num_classes="4" loss="penalty_ent" optim="adam" scheduler="step" wandb.project="unet-cardiac"
+#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="cardiac" model="unet" model.num_classes="4" loss="ls" optim="adam" scheduler="step" wandb.project="unet-cardiac"
+#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="cardiac" model="unet" model.num_classes="4" loss="logit_margin" optim="adam" scheduler="step" wandb.project="unet-cardiac"
