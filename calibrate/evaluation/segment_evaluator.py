85fbdd03efa9bdc7d945f817ae134ea7cfbd70a0
bala93 <balamuralim.1993@gmail.com>
2022-04-21 14:57:32 -0400

    Add hd feature

--- calibrate/evaluation/segment_evaluator.py
+++ calibrate/evaluation/segment_evaluator.py
@@ -4,9 +4,11 @@ from terminaltables import AsciiTable
 import pandas as pd
 from typing import List, Optional
 import wandb
+from medpy.metric import binary
 
 from .evaluator import DatasetEvaluator
 from calibrate.utils.constants import EPS
+
 
 logger = logging.getLogger(__name__)
 
@@ -30,6 +32,20 @@ def intersect_and_union(pred_label, label, num_classes, ignore_index):
 
     return area_intersect, area_union, area_pred_label, area_label
 
+def shape_metrics(pred_label, label, num_classes):
+
+    hd = []
+
+    for cls in range(num_classes):
+        pred_bw = pred_label == cls
+        label_bw = label == cls
+        if np.sum(pred_bw) & np.sum(label_bw):
+            hd.append(binary.hd(pred_bw, label_bw))
+        else:
+            hd.append(0)
+
+    return hd
+
 
 class SegmentEvaluator(DatasetEvaluator):
     def __init__(self,
@@ -39,6 +55,7 @@ class SegmentEvaluator(DatasetEvaluator):
         self.classes = classes
         self.num_classes = len(self.classes)
         self.ignore_index = ignore_index
+        self.ishd = False
 
     def num_samples(self):
         return self.nsamples
@@ -48,6 +65,7 @@ class SegmentEvaluator(DatasetEvaluator):
         self.total_area_union = np.zeros((self.num_classes, ), dtype=np.float)
         self.total_area_pred = np.zeros((self.num_classes, ), dtype=np.float)
         self.total_area_target = np.zeros((self.num_classes, ), dtype=np.float)
+        self.total_hd = np.zeros((self.num_classes, ), dtype=np.float)
         self.nsamples = 0
 
     def main_metric(self):
@@ -73,25 +91,34 @@ class SegmentEvaluator(DatasetEvaluator):
         batch_area_union = np.zeros((self.num_classes, ), dtype=np.float)
         batch_area_pred = np.zeros((self.num_classes, ), dtype=np.float)
         batch_area_target = np.zeros((self.num_classes, ), dtype=np.float)
+        batch_area_target = np.zeros((self.num_classes, ), dtype=np.float)
+        batch_hd = np.zeros((self.num_classes, ), dtype=np.float)
+        
         for i in range(n):
             area_inter, area_union, area_pred, area_target = (
                 intersect_and_union(
                     pred[i], target[i], self.num_classes, self.ignore_index
                 )
             )
+            
             batch_area_inter += area_inter
             batch_area_union += area_union
             batch_area_pred += area_pred
             batch_area_target += area_target
 
+            if self.ishd:
+                hd = shape_metrics(pred[i], target[i], self.num_classes)
+                batch_hd += hd
+
         iou = batch_area_inter[1:].sum() / (batch_area_union[1:].sum() + EPS)
         self.curr = {"iou": iou}
-
+        
         # update the total
         self.total_area_inter += batch_area_inter
         self.total_area_union += batch_area_union
         self.total_area_pred += batch_area_pred
         self.total_area_target += batch_area_target
+        self.total_hd += batch_hd
 
     def curr_score(self):
         return self.curr
@@ -107,19 +134,25 @@ class SegmentEvaluator(DatasetEvaluator):
         macc = (
             self.total_area_inter[1:] / (self.total_area_target[1:] + EPS)
         ).mean()
+
+        mhd = (self.total_hd[1:] / self.nsamples).mean()
+
         if main:
             return miou
         else:
-            return {"mdsc": mdice, "miou": miou, "macc": macc}
+            return {"mdsc": mdice, "miou": miou, "macc": macc, "mhd": mhd}
 
-    def class_score(self, print=True, return_dataframe=False):
+    def class_score(self, isprint=True, return_dataframe=False):
         class_acc = self.total_area_inter[1:] / (self.total_area_target[1:] + EPS)
         class_dice = (
             2 * self.total_area_inter[1:]
             / (self.total_area_pred[1:] + self.total_area_target[1:] + EPS)
         )
         class_iou = self.total_area_inter[1:] / (self.total_area_union[1:] + EPS)
-        columns = ["id", "Class", "iou", "dsc", "acc"]
+        
+        class_hd = self.total_hd[1:] / self.nsamples
+        columns = ["id", "Class", "iou", "dsc", "acc", "hd"]
+
         class_table_data = [columns]
         for i in range(class_acc.shape[0]):
             class_table_data.append(
@@ -127,15 +160,17 @@ class SegmentEvaluator(DatasetEvaluator):
                 + ["{:.4f}".format(class_iou[i])]
                 + ["{:.4f}".format(class_dice[i])]
                 + ["{:.4f}".format(class_acc[i])]
+                + ["{:.4f}".format(class_hd[i])]
             )
         class_table_data.append(
             [""] + ["mean"]
             + ["{:.4f}".format(np.mean(class_iou))]
             + ["{:.4f}".format(np.mean(class_dice))]
             + ["{:.4f}".format(np.mean(class_acc))]
+            + ["{:.4f}".format(np.mean(class_hd))]
         )
 
-        if print:
+        if isprint:
             table = AsciiTable(class_table_data)
             logger.info("\n" + table.table)
 
@@ -147,13 +182,17 @@ class SegmentEvaluator(DatasetEvaluator):
                 data[columns[3]].append(class_iou[i])
                 data[columns[2]].append(class_dice[i])
                 data[columns[4]].append(class_acc[i])
+                data[columns[5]].append(class_hd[i])
+                
             data[columns[0]].append(None)
             data[columns[1]].append("mean")
             data[columns[3]].append(np.mean(class_iou))
             data[columns[2]].append(np.mean(class_dice))
             data[columns[4]].append(np.mean(class_acc))
+            data[columns[5]].append(np.mean(class_hd))
+
             return pd.DataFrame(data, columns=columns)
 
     def wandb_score_table(self):
-        table_data = self.class_score(print=False, return_dataframe=True)
+        table_data = self.class_score(isprint=False, return_dataframe=True)
         return wandb.Table(dataframe=table_data)
