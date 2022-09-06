5bf174350080da86a3f2b586b8672a978a430555
bala93 <balamuralim.1993@gmail.com>
2022-09-06 18:15:07 -0400

    Clearning code by deleting

--- train_mmcardiac.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="mmcardiac" model="unet" model.num_classes="4" loss="ce" optim="adam" scheduler="step" wandb.project="unet-mmcardiac"
-CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="mmcardiac" model="unet" model.num_classes="4" loss="ls" optim="adam" scheduler="step" wandb.project="unet-mmcardiac"
-CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="mmcardiac" model="unet" model.num_classes="4" loss="logit_margin" optim="adam" scheduler="step" wandb.project="unet-mmcardiac"
-#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="cardiac" model="unet" model.num_classes="4" loss="focal" optim="adam" scheduler="step" wandb.project="unet-cardiac"
-#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="cardiac" model="unet" model.num_classes="4" loss="penalty_ent" optim="adam" scheduler="step" wandb.project="unet-cardiac"
-#
