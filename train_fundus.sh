8b01466a4d8302884eb4edb07a63d6144d090679
bala93 <balamuralim.1993@gmail.com>
2022-09-06 18:17:30 -0400

    Clearning code by deleting

--- train_fundus.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="fundus" data.batch_size="4" model="unet" model.num_inp_channels="3" model.num_classes="3" loss="ce" optim="adam" scheduler="step" wandb.project="unet-fundus" train.max_epoch="100" log_period="100"
-# CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="fundus" data.batch_size="16" model="unet" model.num_inp_channels="1" model.num_classes="3" loss="ls" optim="adam" scheduler="step" wandb.project="unet-fundus" train.max_epoch="100" log_period="100"
-# CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="fundus" data.batch_size="16" model="unet" model.num_inp_channels="1" model.num_classes="3" loss="logit_margin" optim="adam" scheduler="step" wandb.project="unet-fundus" train.max_epoch="100" log_period="100"
-
-
-#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="fundus" model="unet" loss="focal" optim="adam" scheduler="step" wandb.project="unet-fundus"
-#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="fundus" model="unet" loss="penalty_ent" optim="adam" scheduler="step" wandb.project="unet-fundus"
--- train_kidney.sh
+++
@@ -1,7 +0,0 @@
-#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="kidney" data.batch_size="16" model="unet" model.num_inp_channels="1" model.num_classes="3" loss="ce" optim="adam" scheduler="step" wandb.project="unet-kidney" train.max_epoch="100" log_period="100"
-CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="kidney" data.batch_size="16" model="unet" model.num_inp_channels="1" model.num_classes="3" loss="ls" optim="adam" scheduler="step" wandb.project="unet-kidney" train.max_epoch="100" log_period="100"
-CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="kidney" data.batch_size="16" model="unet" model.num_inp_channels="1" model.num_classes="3" loss="logit_margin" optim="adam" scheduler="step" wandb.project="unet-kidney" train.max_epoch="100" log_period="100"
-
-
-#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="kidney" model="unet" loss="focal" optim="adam" scheduler="step" wandb.project="unet-kidney"
-#CUDA_VISIBLE_DEVICES=4 python tools/train_net.py wandb.enable=True task="medseg" data="kidney" model="unet" loss="penalty_ent" optim="adam" scheduler="step" wandb.project="unet-kidney"
