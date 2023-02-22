65f1230e966bc8689e591a6a83286ec7c864ab70
bala93 <balamuralim.1993@gmail.com>
2023-02-21 22:22:18 -0500

    Add Gaussian kernel utils

--- calibrate/losses/utils.py
+++ calibrate/losses/utils.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn.functional as F
 import math
 
+'''
 def get_svls_filter_2d(kernel_size=3, sigma=1, channels=4):
     # Create a x, y, z coordinate grid of shape (kernel_size, kernel_size, kernel_size, 3)
     x_coord = torch.arange(kernel_size)
@@ -43,3 +45,35 @@ def get_svls_filter_2d(kernel_size=3, sigma=1, channels=4):
     svls_filter_2d.weight.requires_grad = False 
     
     return svls_filter_2d, svls_kernel_2d[0]
+    
+'''
+
+
+def get_gaussian_kernel_2d(ksize=3, sigma=1):
+    x_grid = torch.arange(ksize).repeat(ksize).view(ksize, ksize)
+    y_grid = x_grid.t()
+    xy_grid = torch.stack([x_grid, y_grid], dim=-1).float()
+    mean = (ksize - 1)/2.
+    variance = sigma**2.
+    gaussian_kernel = (1./(2.*math.pi*variance + 1e-16)) * torch.exp( 
+        -torch.sum((xy_grid - mean)**2., dim=-1) / (2*variance + 1e-16)
+        )
+    return gaussian_kernel / torch.sum(gaussian_kernel)
+
+class get_svls_filter_2d(torch.nn.Module):
+    def __init__(self, ksize=3, sigma=1, channels=0):
+        super(get_svls_filter_2d, self).__init__()
+        gkernel = get_gaussian_kernel_2d(ksize=ksize, sigma=sigma)
+        neighbors_sum = (1 - gkernel[1,1]) + 1e-16
+        gkernel[int(ksize/2), int(ksize/2)] = neighbors_sum
+        self.svls_kernel = gkernel / neighbors_sum
+        svls_kernel_2d = self.svls_kernel.view(1, 1, ksize, ksize)
+        svls_kernel_2d = svls_kernel_2d.repeat(channels, 1, 1, 1)
+        padding = int(ksize/2)
+        self.svls_layer = torch.nn.Conv2d(in_channels=channels, out_channels=channels,
+                                    kernel_size=ksize, groups=channels,
+                                    bias=False, padding=padding, padding_mode='replicate')
+        self.svls_layer.weight.data = svls_kernel_2d
+        self.svls_layer.weight.requires_grad = False
+    def forward(self, x):
+        return self.svls_layer(x) / self.svls_kernel.sum()
